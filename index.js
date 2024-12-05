3f8e6a8a9ef234a71187905a9c70d0a774eebe51
eli <john.berengu@student.moringaschool.com>
2024-12-05 20:20:30 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+// const names = ["Guadalupe", "Ollie", "Aki"]
+// const writeCards = () => {
+//     for (let i = 0; i < names.length; i++) {
+          
+//     }
+// }
+const writeCards = (names, event) => {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+  const countDown = () => {
+    for (let i = 10; i >= 0 ; i--) {
+        console.log(i);
+        
+    }
+  };
+  countDown()
+  
+  
